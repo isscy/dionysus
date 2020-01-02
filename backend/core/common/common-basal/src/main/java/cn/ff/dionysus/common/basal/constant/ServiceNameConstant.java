479d5976854e86faa5234e4f0e2a70444bcc3d9a
fengfan <dotff@qq.com>
2020-01-02 08:43:02 +0800

    [prepare] common service

---
+++ backend/core/common/common-basal/src/main/java/cn/ff/dionysus/common/basal/constant/ServiceNameConstant.java
@@ -0,0 +1,17 @@
+package cn.ff.dionysus.common.basal.constant;
+
+/**
+ * 微服务名称
+ *
+ * @author fengfan 2019/12/31
+ */
+public interface ServiceNameConstant {
+
+
+    String SERVICE_AUTH = "auth";
+
+    String SERVICE_GATEWAY = "gateway";
+
+
+    // TODO 1 : 数据权限  https://gitee.com/log4j/pig/blob/master/pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/mybatis/DataScopeInterceptor.java
+}
